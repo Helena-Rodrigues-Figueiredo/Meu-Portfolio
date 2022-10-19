c70cf42dadf57e400b733a545ca9ffbf3ca86704
Helena Rodrigues Figueiredo <helena.asrodrigues@gmail.com>
2022-10-19 18:04:20 -0300

    alterações na estilização das divs de cada projeto

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,9 @@
 import styles from '../styles/Projects.module.css';
 import Pulse from 'react-reveal/Pulse';
+import trivia from '../assets/images/Trivia.ico'
+import trybewallet from '../assets/images/Trybewallet.png'
+import pixelArt from '../assets/images/PixelArt.png'
+import trybetunes from '../assets/images/Trybetunes.webp'
 
 export default function Projects() {
   return (
@@ -9,24 +13,32 @@ export default function Projects() {
         <Pulse>
           <div className={styles.containerprojects}>
             <div className={styles.project}>
-              <div className={styles.imgproject}>Imagem do Projeto</div>
-              <h2 className={styles.h2project}>Em breve</h2>
-              <p className={styles.pproject}>Conhecimento utilizado</p>
+              <div className={styles.imgproject}>
+                <img src={ trivia } alt='imagem aplicação trivia' />
+              </div>
+              <h2 className={styles.h2project}>Trivia Game</h2>
+              <button className={styles.buttonproject}>Detalhes</button>
             </div>
             <div className={styles.project}>
-              <div className={styles.imgproject}>Imagem do Projeto</div>
-              <h2 className={styles.h2project}>Em breve</h2>
-              <p className={styles.pproject}>Conhecimento utilizado</p>
+              <div className={styles.imgproject}>
+              <img src={ trybewallet } alt='imagem aplicação trybewallet' />
+              </div>
+              <h2 className={styles.h2project}>Trybewallet</h2>
+              <button className={styles.buttonproject}>Detalhes</button>
             </div>
             <div className={styles.project}>
-              <div className={styles.imgproject}>Imagem do Projeto</div>
-              <h2 className={styles.h2project}>Em breve</h2>
-              <p className={styles.pproject}>Conhecimento utilizado</p>
+              <div className={styles.imgproject}>
+              <img src={ pixelArt } alt='imagem aplicação pixel art' />
+              </div>
+              <h2 className={styles.h2project}>Pixel Art</h2>
+              <button className={styles.buttonproject}>Detalhes</button>
             </div>
             <div className={styles.project}>
-              <div className={styles.imgproject}>Imagem do Projeto</div>
-              <h2 className={styles.h2project}>Em breve</h2>
-              <p className={styles.pproject}>Conhecimento utilizado</p>
+              <div className={styles.imgproject}>
+              <img src={ trybetunes } alt='imagem aplicação trybetunes' />
+              </div>
+              <h2 className={styles.h2project}>Trybetunes</h2>
+              <button className={styles.buttonproject}>Detalhes</button>
             </div>
           </div>
         </Pulse>
